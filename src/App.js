4834e37604d6fe97873399be93c9ead038689f71
Aneesha Kommineni <aneeshak@Aneeshas-MacBook-Pro.local>
2019-07-29 10:57:46 -0700

    trying styled & themed treat examples

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button } from './Button';
+import { TreatProvider } from 'react-treat';
+import theme from './theme.treat.js';
+import { ThemedButton } from './Button-theme';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button text="hi"></Button>
+      <TreatProvider theme={theme}><ThemedButton>hello</ThemedButton></TreatProvider>
     </div>
   );
 }
---
+++ src/Button.js
@@ -0,0 +1,5 @@
+import * as styles from './Button.treat.js';
+
+export const Button = ({ text }) => `
+  <button class="${styles.button}">${text}</button>
+`;
